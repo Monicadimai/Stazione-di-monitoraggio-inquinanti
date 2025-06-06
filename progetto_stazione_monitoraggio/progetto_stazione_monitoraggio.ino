75f763e84a0f5d1a93288fed130378d88e86152d
Monicadimai <147213909+Monicadimai@users.noreply.github.com>
2025-06-06 12:06:12 +0200

    Progetto_stazione_monitoraggio.ino

--- progetto_stazione_monitoraggio/progetto_stazione_monitoraggio.ino
+++ progetto_stazione_monitoraggio/progetto_stazione_monitoraggio.ino
@@ -10,7 +10,6 @@
 COZIR czr (&Serial1);  //  RX, TX, optional inverse logic
 
 
-
 #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
   #include <WiFiNINA.h>
 #elif defined(ARDUINO_SAMD_MKR1000)
@@ -28,6 +27,7 @@ COZIR czr (&Serial1);  //  RX, TX, optional inverse logic
 #include "arduino_secrets.h"  // Definisci SECRET_SSID e SECRET_PASS
 
 #define SERIAL_BAUD 115200
+#define DO_COZIR_CONFIGURATION false // metti a true solo la prima volta per configurare il range
 
 struct valori{
     float temp;
@@ -52,12 +52,22 @@ valori misure;
 struct sps30_measurement m;
 uint16_t data_ready;
 int16_t ret;
+uint32_t c;
 
 int sogliaCO2= 2000;
 int sogliaTemp=35;
 int sogliaPress=1030;
 int sogliaHum=70;
+int sogliaParticleSize=2.5;
+int sogliaPM1_0=20;
 int sogliaPM2_5=25;
+int sogliaPM4_0=50;
+int sogliaPM10_0=50;
+int sogliaNC0_5=700;
+int sogliaNC1_0=500;
+int sogliaNC2_5=400;
+int sogliaNC4_0=300;
+int sogliaNC10_0=150;
 
 BME280I2C bme;
 WiFiClient wifiClient;
@@ -74,28 +84,23 @@ const char topic4[] = "arduino/ALLARME";
 const long interval = 1000;
 unsigned long previousMillis = 0;
 
-
-
 void setup() {
   Wire.begin();
 
   Serial1.begin(9600);
+  Serial.begin(115200);
+  while (!Serial) {}
 
   czr.init();
 
+  czr.setOperatingMode(CZR_POLLING);  // Torna in modalità polling
+  delay(500);
 
-  Serial.begin(115200);
   Serial.print("COZIR_LIB_VERSION: ");
   Serial.println(COZIR_LIB_VERSION);
   Serial.println();
 
-  
-  czr.setOperatingMode(CZR_POLLING);
-  delay(1000);
-
-  while (!Serial) {}
-
-  //BME280 INIT
+  // Inizializzazione BME280
   while (!bme.begin()) {
     Serial.println("Could not find BME280 sensor!");
     delay(1000);
@@ -108,40 +113,40 @@ void setup() {
     case BME280::ChipModel_BMP280:
       Serial.println("Found BMP280 sensor (no humidity).");
       break;
-     default:
-       Serial.println("Unknown sensor detected!");
-   }
+    default:
+      Serial.println("Unknown sensor detected!");
+  }
 
-   //SPS30 INIT
-   sensirion_i2c_init();
-   while (sps30_probe() != 0) {
-     Serial.println("SPS sensor probing failed");
-     delay(500);
-   }
+  // Inizializzazione SPS30
+  sensirion_i2c_init();
+  while (sps30_probe() != 0) {
+    Serial.println("SPS sensor probing failed");
+    delay(500);
+  }
   Serial.println("SPS sensor probing successful");
 
-   sps30_set_fan_auto_cleaning_interval_days(4);
-   sps30_start_measurement();
-   Serial.println("SPS30 measurements started");
+  sps30_set_fan_auto_cleaning_interval_days(4);
+  sps30_start_measurement();
+  Serial.println("SPS30 measurements started");
 
-   //WIFI CONNECTION
+  // Connessione WiFi
   Serial.print("Connecting to SSID: ");
-   Serial.println(SECRET_SSID);
-   while (WiFi.begin(SECRET_SSID, SECRET_PASS) != WL_CONNECTED) {
-     Serial.print(".");
-     delay(5000);
-   }
-   Serial.println("\nConnected to WiFi");
+  Serial.println(SECRET_SSID);
+  while (WiFi.begin(SECRET_SSID, SECRET_PASS) != WL_CONNECTED) {
+    Serial.print(".");
+    delay(5000);
+  }
+  Serial.println("\nConnected to WiFi");
 
-   //BROKER CONNECTION
-   Serial.print("Connecting to MQTT broker: ");
-   Serial.println(broker);
-   while (!mqttClient.connect(broker, port)) {
+  // Connessione broker MQTT
+  Serial.print("Connecting to MQTT broker: ");
+  Serial.println(broker);
+  while (!mqttClient.connect(broker, port)) {
     Serial.print("MQTT connection failed. Error = ");
     Serial.println(mqttClient.connectError());
-     Serial.println("Retrying in 5 seconds..");
-   delay(5000);
-   }
+    Serial.println("Retrying in 5 seconds..");
+    delay(5000);
+  }
   Serial.println("Connected to MQTT broker");
 }
 
@@ -161,7 +166,7 @@ void loop() {
      payload1 += "\"TEMPERATURE\":" + String(misure.temp, 2) + ",";
      payload1 += "\"HUMIDITY\":" + String(misure.hum, 2) + ",";
      payload1 += "\"PRESSURE\":" + String(misure.pres, 2);
-    payload1 += "}";
+     payload1 += "}";
      Serial.println(payload1); 
      mqttClient.print(payload1);
      mqttClient.endMessage();
@@ -169,34 +174,29 @@ void loop() {
 
      mqttClient.beginMessage(topic2);
      String payload2 = "{";
-     payload2 += "\"PM1_0\":" + String(m.mc_1p0, 3) + ",";
-     payload2 += "\"PM2_5\":" + String(m.mc_2p5, 3) + ",";
-     payload2 += "\"PM4_0\":" + String(m.mc_4p0, 3) + ",";
-     payload2 += "\"PM10_0\":" + String(m.mc_10p0, 3) + ",";
-     payload2 += "\"NC0_5\":" + String(m.nc_0p5, 3) + ",";
-     payload2 += "\"NC1_0\":" + String(m.nc_1p0, 3) + ",";
-     payload2 += "\"NC2_5\":" + String(m.nc_2p5, 3) + ",";
-     payload2 += "\"NC4_0\":" + String(m.nc_4p0, 3) + ",";
-     payload2 += "\"NC10_0\":" + String(m.nc_10p0, 3) + ",";
-     payload2 += "\"Typical_particle_size\":" + String(m.typical_particle_size, 2);
+     payload2 += "\"PM1_0\":" + String(misure.PM1_0, 3) + ",";
+     payload2 += "\"PM2_5\":" + String(misure.PM2_5, 3) + ",";
+     payload2 += "\"PM4_0\":" + String(misure.PM4_0, 3) + ",";
+     payload2 += "\"PM10_0\":" + String(misure.PM10_0, 3) + ",";
+     payload2 += "\"NC0_5\":" + String(misure.NC0_5, 3) + ",";
+     payload2 += "\"NC1_0\":" + String(misure.NC1_0, 3) + ",";
+     payload2 += "\"NC2_5\":" + String(misure.NC2_5, 3) + ",";
+     payload2 += "\"NC4_0\":" + String(misure.NC4_0, 3) + ",";
+     payload2 += "\"NC10_0\":" + String(misure.NC10_0, 3) + ",";
+     payload2 += "\"Typical_particle_size\":" + String(misure.typical_particle_size, 2);
      payload2 += "}";
      Serial.println(payload2); 
      mqttClient.print(payload2);
      mqttClient.endMessage();
 
-    uint32_t c = czr.CO2();
-    misure.CO2=czr.CO2();
     Serial.print("CO2 =\t");
-    Serial.println(c);
+    Serial.println(misure.CO2);
     delay(1000);
-     //TOPIC CO2
-     mqttClient.beginMessage(topic3);
-     mqttClient.print(c);
+    //TOPIC CO2
+    mqttClient.beginMessage(topic3);
+    mqttClient.print(misure.CO2);
     mqttClient.endMessage();
- 
- 
- 
- 
+
     checkAlarm();
     
    }
@@ -205,31 +205,32 @@ void loop() {
 void checkAlarm()
 {
   String payload = "{";
-  payload += "\"PM1_0\":" + String(misure.PM1_0 > 10) + ",";
-  payload += "\"PM2_5\":" + String(misure.PM2_5 > 10) + ",";
-  payload += "\"PM4_0\":" + String(misure.PM4_0 > 10) + ",";
-  payload += "\"NC10_0\":" + String(misure.PM10_0 > 10) + ",";
-  payload += "\"NC0_5\":" + String(misure.NC0_5 > 10) + ",";
-  payload += "\"NC1_0\":" + String(misure.NC1_0 > 10) + ",";
-  payload += "\"NC2_5\":" + String(misure.NC2_5 > 10) + ",";
-  payload += "\"NC4_0\":" + String(misure.NC4_0 > 10) + ",";
-  payload += "\"NC10_0\":" + String(misure.NC10_0 > 10) + ",";
-  payload += "\"TEMPERATURE\":" + String(misure.temp > 10) + ",";
-  payload += "\"PRESSURE\":" + String(misure.pres> 10) + ",";
-  payload += "\"HUMIDITY\":" + String(misure.hum > 10) + ",";
-  payload += "\"CO2\":" + String(misure.CO2 > 10) + ",";
-  payload += "\"Typical_particle_size\":" + String(misure.typical_particle_size > 10);
+  payload += "\"PM1_0\":" + String(misure.PM1_0 > sogliaPM1_0) + ",";
+  payload += "\"PM2_5\":" + String(misure.PM2_5 > sogliaPM2_5) + ",";
+  payload += "\"PM4_0\":" + String(misure.PM4_0 > sogliaPM4_0) + ",";
+  payload += "\"NC10_0\":" + String(misure.PM10_0 > sogliaPM10_0) + ",";
+  payload += "\"NC0_5\":" + String(misure.NC0_5 > sogliaNC0_5) + ",";
+  payload += "\"NC1_0\":" + String(misure.NC1_0 > sogliaNC1_0) + ",";
+  payload += "\"NC2_5\":" + String(misure.NC2_5 > sogliaNC2_5) + ",";
+  payload += "\"NC4_0\":" + String(misure.NC4_0 > sogliaNC4_0) + ",";
+  payload += "\"NC10_0\":" + String(misure.NC10_0 > sogliaNC10_0) + ",";
+  payload += "\"TEMPERATURE\":" + String(misure.temp > sogliaTemp) + ",";
+  payload += "\"PRESSURE\":" + String(misure.pres> sogliaPress) + ",";
+  payload += "\"HUMIDITY\":" + String(misure.hum > sogliaHum) + ",";
+  payload += "\"CO2\":" + String(misure.CO2 > sogliaCO2) + ",";
+  payload += "\"Typical_particle_size\":" + String(misure.typical_particle_size > sogliaParticleSize);
   payload += "}";
 
   mqttClient.beginMessage(topic4);
-     mqttClient.print(payload);
-    mqttClient.endMessage();
+  mqttClient.print(payload);
+  mqttClient.endMessage();
 }
   
 void readData(){
 
  //lettura BME280
   bme.read(misure.pres, misure.temp, misure.hum, BME280::TempUnit_Celsius, BME280::PresUnit_Pa);
+  misure.pres/=100;
 
   //lettura SPS30
   do {
@@ -242,7 +243,7 @@ void readData(){
     }
   } while (!data_ready);
 
-  ret = sps30_read_measurement(&m);
+  ret=sps30_read_measurement(&m);
   misure.PM1_0=m.mc_1p0;
   misure.PM2_5=m.mc_2p5;
   misure.PM4_0=m.mc_4p0;
@@ -256,38 +257,21 @@ void readData(){
 
   misure.typical_particle_size=m.typical_particle_size;
   
-
-
-
-
-
+  misure.CO2=czr.CO2();
 
   if (ret < 0) {
     Serial.println("Error reading measurement");
   } else {
   }
-
 }
 
-
 void printData(Stream* client){
-
   client->print("Temp: ");
   client->print(misure.temp);
   client->print(" °C\tHumidity: ");
   client->print(misure.hum);
   client->print(" %\tPressure: ");
   client->print(misure.pres);
-  client->println("Pa");
+  client->println("hPa");
 }
 
-void sendCommand(String cmd) {
-  Serial1.print(cmd);     // Usa direttamente Serial1
-  Serial1.print("\r");    // Carriage return richiesto
-  delay(500);             // Attendi la risposta del sensore
-
-  // Stampa la risposta ricevuta (opzionale, per debug)
-  while (Serial1.available()) {
-    Serial.write(Serial1.read());
-}
-}
